35d1f4c8660b3477b7b9cb5d2ea716e6ddd19516
AiandioIfidon <150620311+AiandioIfidon@users.noreply.github.com>
2025-07-18 13:05:59 +0100

    Addding the bluetooth functionalitu

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import {BLEservice} from "@/components/BLEservice";
+
+export default function Index() {
+  return (
+    <View>
+      <BLEservice />
+    </View>
+  );
+}
---
+++ components/BLEservice.jsx
@@ -0,0 +1,16 @@
+import { bleManager } from "../utils/BLEinstance";
+import { View, Text, StyleSheet, TextInput, SafeAreaView } from "react-native-web";
+import { useState, useEffect } from "react";
+
+const BLEservice = () => {
+    return(
+        <SafeAreaView>
+            <View>
+                <Text>Bluetooth page</Text>
+            </View>
+        </SafeAreaView>
+    );
+}
+
+
+export default BLEservice;
