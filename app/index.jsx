b810d02da5b0ca4d417fd8edf1ecfc680f174f14
AiandioIfidon <150620311+AiandioIfidon@users.noreply.github.com>
2025-07-18 21:35:18 +0100

    First commit

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+
+import { View, Text, SafeAreaView } from "react-native";
+
+export default function Index() {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View>
+        <Text>Hello there</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
